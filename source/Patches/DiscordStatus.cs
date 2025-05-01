7a27afe59e5f984ddf8415ff178162b70a101b37
badzyn <filip46666@gmail.com>
2025-05-02 00:29:06 +0200

    1.0.0

--- source/Patches/DiscordStatus.cs
+++ source/Patches/DiscordStatus.cs
@@ -9,7 +9,7 @@ namespace TownOfUs.Patches
         [HarmonyPrefix]
         public static void Prefix([HarmonyArgument(0)] Activity activity)
         {
-            activity.Details += $" Town of Us v{TownOfUs.VersionString}";
+            activity.Details += $" Town of Us Unofficial v{TownOfUs.VersionString}";
         }
     }
 }
