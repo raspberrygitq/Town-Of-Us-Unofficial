435f2ba7c3d0538376f6edbebf09899b7bc01820
badzyn <filip46666@gmail.com>
2025-05-02 23:06:37 +0200

    Update ShowHostMeetingPatch.cs

--- source/Patches/ShowHostMeetingPatch.cs
+++ source/Patches/ShowHostMeetingPatch.cs
@@ -19,7 +19,7 @@ namespace TownOfUs.Patches
             if (host != null)
             {
                 PlayerMaterial.SetColors(host.DefaultOutfit.ColorId, __instance.HostIcon);
-                __instance.ProceedButton.gameObject.GetComponentInChildren<TextMeshPro>().text = $"host: {host.PlayerName}";
+                __instance.ProceedButton.gameObject.GetComponentInChildren<TextMeshPro>().text = ": VOTE LOB!";
             }
         }
 
