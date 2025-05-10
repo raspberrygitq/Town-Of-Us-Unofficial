19191432daed534b896af57c6bc985e695724a8c
badzyn <filip46666@gmail.com>
2025-05-10 02:11:13 +0200

    v1.2.2

--- source/Patches/VersionShowerUpdate.cs
+++ source/Patches/VersionShowerUpdate.cs
@@ -10,7 +10,7 @@ namespace TownOfUs
         public static void Postfix(VersionShower __instance)
         {
             var text = __instance.text;
-            text.text += " - <color=#00FF00FF>TownOfUsUnofficial v" + TownOfUs.VersionString + "</color>" + TownOfUs.VersionTag;
+            text.text += " - <color=#00FF00FF>TownOfUs<color=#FF0000>Unofficial</color> v" + TownOfUs.VersionString + "</color>" + TownOfUs.VersionTag;
             text.transform.localPosition += new Vector3(-0.8f, -0.16f, 0f);
 
             if (GameObject.Find("RightPanel"))
